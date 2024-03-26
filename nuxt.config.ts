ba9d3a6ce7352faec85b27c2cd080030e78d27a9
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-03-26 21:02:35 +0100

    eslint-vueuse added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true,
-  timeline: {
-    enabled: true
-  }
- },
- modules: [
-  '@nuxtjs/tailwindcss',
-  '@nuxtjs/color-mode',
-  '@nuxt/content'
- ]
-})
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
+  css: ["/layouts/global.css"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+    "@vueuse/nuxt",
+  ],
+});
